1468f4ae9768d504db79e2e5f9045ef4fccba38a
flower <460380617@qq.com>
2020-08-31 17:01:18 +0800

    create project

---
+++ fertilizer/src/main/java/com/fertilizer/yh/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.fertilizer.yh;
+
+/**
+ * @className HelloWorld
+ * @package com.fertilizer.yh
+ * @describe create project first class
+ * @author hua.yang
+ * @date 2020/8/31 16:59:00
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
